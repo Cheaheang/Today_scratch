55cc9d4a08d61b37df8b19e93d09284bf645d88d
Cheaheang <cheaheangchoeng@gmail.com>
2024-05-30 06:21:14 +0700

    fix late load

--- ui/customer/card.php
+++ ui/customer/card.php
@@ -12,6 +12,20 @@
 </style>
 
 <body style="background-color: darkgray;display: flex;justify-content: center;align-items: center;  ">
+
+    <script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"></script>
+    <script src="npm_service.js"></script>
+    <script src='http://code.jquery.com/jquery-3.6.1.min.js' type='text/javascript'></script>
+    <script src='jquery.eraser.js'></script>
+    <script type="text/javascript">
+        $(function() {
+            $('#redux').eraser({
+                progressFunction: function(p) {
+                    $('#progress').html(Math.round(p * 100) + '%');
+                }
+            });
+        });
+    </script>
     <?php
     $conn = mysqli_connect("localhost", "root", "", "scratcher");
     if ($conn === false) {
@@ -40,32 +54,19 @@
 
                     <span class="container">
                          
-                    
-
+                        <img id='redux' src='../img/sratcher.jpg' />
                         <p id="robot" style="display: flex; align-items: center ;  justify-content: center;font-size:50px ;">
                             <?php
-                            $fiber = new Fiber(function (): void {
-                                $value = Fiber::suspend();
-                                $conn = mysqli_connect("localhost", "root", "", "scratcher");
-                                if ($conn === false) {
-                                    die("ERROR: Could not connect" . mysqli_connect_error());
-                                } else {
-                                    echo ("");
-                                }
-                                $code  = $_GET['userInput'];
-                                $result = $conn->query("SELECT * FROM `user` WHERE pinCode = $code");
-                                $row = $result->fetch_assoc();
-                                $reward = $row['reward'];
-                                echo $reward . " Bandwidth", PHP_EOL;
-                            });
-                            $value = $fiber->start();
-                            
-                            echo " <img id='redux' src='../img/sratcher.jpg' />", $value, PHP_EOL;
-                            
-                            $fiber->resume('');
+                            $code  = $_GET['userInput'];
+                            $reward;
+                            $result = $conn->query("SELECT * FROM `user` WHERE pinCode = $code");
+                            $row = $result->fetch_assoc();
+                            $pinCode = $row["pinCode"];
+                            $reward = $row['reward'];
+                            echo $reward . " Bandwidth";
                             ?>
-                            </p>
-                        </>
+                        </p>
+                       
                     </span>
                 </div>
                 <div style="padding-top: 21px;">
@@ -83,19 +84,6 @@
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"></script>
-    <script src="npm_service.js"></script>
-    <script src='http://code.jquery.com/jquery-3.6.1.min.js' type='text/javascript'></script>
-    <script src='jquery.eraser.js'></script>
-    <script type="text/javascript">
-        $(function() {
-            $('#redux').eraser({
-                progressFunction: function(p) {
-                    $('#progress').html(Math.round(p * 100) + '%');
-                }
-            });
-        });
-    </script>
     <script>
         <?php
         // $name =$_REQUEST['username'];
